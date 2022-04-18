04a48abcb5371f2ec034328ad46f2197a6beaf25
Alexis-Fourthies <alexisfourthies@gmail.com>
2022-04-18 19:45:29 +0200

    deuxième commit

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,54 +1,27 @@
 def translate(word)
-  vowels = ['a', 'e', 'i', 'o', 'u']
-  consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-    if vowels.include?(word[0])
-      word + 'ay'
-    elsif consonants.include?(word[0])
-      word[1..-1] + word[0] + 'ay'
-    elsif consonants.include?(word[0]) && consonants.include?(word[1])
-      word[2..-1] + word[0..1] + 'ay'
-    elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
-      word[3..-1] + word[0..2] + 'ay'  
-    end
-  end
+  alpha = ('a'..'z').to_a
+  vowels = %w[a e i o u]
+  consonants = alpha - vowels
 
-def translate(phrase)
-  words = phrase.split(' ')
-  words.map! { |word| translate(word) }
-  words.join(' ')
-end
-
-def translate(phoneme)
-  vowels = ['a', 'e', 'i', 'o', 'u']
-  consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-  if vowels.include?(phoneme[0])
-    phoneme + 'ay'
-  elsif consonants.include?(phoneme[0]) && consonants.include?(phoneme[1])
-    phoneme[2..-1] + phoneme[0..1] + 'ay'
-  elsif consonants.include?(phoneme[0])
-    phoneme[1..-1] + phoneme[0] + 'ay'
+  if vowels.include?(word[0])
+    word + 'ay'
+  elsif consonants.include?(word[0]) && consonants.include?(word[1])
+    word[2..-1] + word[0..1] + 'ay'
+  elsif consonants.include?(word[0])
+    word[1..-1] + word[0] + 'ay'
+  else
+    word 
   end
 end
 
-def translate(phrase)
-    s = phrase.split(" ")
-    s.map! do |word|
-        if word[0] == "a" || word[0] == "e" || word[0] == "i" || word[0] == "o" || word[0] == "u"
-            word + "ay"
-        else
-            word[1..-1] + word[0] + "ay"
-        end
-    end
-
-    return s.join(" ")
+def translate (phrase)
+  vowels = %w{a e i o u}
+  phrase.gsub(/(\A|\s)\w+/) do |word|
+          word.strip!
+      while not vowels.include? word[0] or (word[0] == 'u' and word[-1] == 'q')
+          word += word[0]
+          word = word[1..-1]
+      end
+      word  = ' ' + word + 'ay'
+  end.strip
 end
-
-puts translate("apple")
-puts translate("banana")
-puts translate("cherry")
-puts translate("eat pie")
-puts translate("three")
-puts translate("school")
-puts translate("quiet")
-puts translate("square")
-puts translate("the quick brown fox")
